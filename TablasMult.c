2f4d65fd9364e91315e7e10a4a3e458fdb412ce6
Francisco A Rodríguez G <fran.argrande@hotmail.com>
2020-04-19 19:23:30 -0600

    Mi primer commit

---
+++ TablasMult.c
@@ -0,0 +1,13 @@
+//Proyecto para probar git, tablas de multiplicar del 1 al 10
+#include<stdio.h>
+
+int main(){
+	int tabla,valor = 0,i;
+	
+	printf("Ingrese el numero de la tabla de multiplicar que desea ver: ");
+	scanf("%i",&tabla);
+	
+	printf("\n\nLa tabla del %i es:\n",tabla);
+	
+	return 0;
+}
